882177951d0421cfbfbbb83481ce267525e0f8f9
leminhhaodt <leminhhao12a01@gmail.com>
2023-03-26 09:47:06 +0700

    v1

---
+++ js/homeboxhotdeal.js
@@ -0,0 +1,12 @@
+$('.box-hot-deal-content .product-listing').owlCarousel({
+    autoplay: false,
+    loop: true,
+    infinite:true,
+    nav: true,
+    dots:false,
+    navText:["<img src='./imgs/arrow-left.svg'>",
+    "<img src='./imgs/arrow-right.svg'>"],
+    items: 2.5,
+    margin: 8,
+  });
+  
